c6f659967f1d828a01474ac4b8653e8c2e594258
Igor Lemeshevski <ilemeshevski@gmail.com>
2023-05-06 22:40:19 +0400

    new design update

--- app/page.js
+++ app/page.js
@@ -1,32 +1,55 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import localFont from 'next/font/local';
+
+const myFont = localFont({ src: '../public/HalvarBreit-Lt.woff2' });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Work in progress&nbsp;
-        </p>
-        <div>
-          <a
-            href=""
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          </a>
-        </div>
-      </div>
+      <div className={styles.contentGrid}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/logo.svg"
-          alt="Glossy Logo"
-          width={220}
-          height={41}
-          priority
-        />
+        <div className={styles.imageBlock}>
+          <Image
+            className={styles.logo}
+            src="/l_b_1.svg"
+            alt="Glossy Logo"
+            layout='fill'
+            objectFit='contain'
+            priority
+          />
+        </div>
+        <div className={styles.rightContentBlock}>
+          <Image
+            className={styles.logo}
+            src="/r_b_1.svg"
+            alt="Glossy Logo"
+            layout='fill'
+            objectFit='contain'
+            priority
+          />
+
+          <Image
+            className={styles.logo}
+            src="/r_b_2.svg"
+            alt="Glossy Logo"
+            layout='fill'
+            objectFit='contain'
+          />
+          <div className={myFont.className}>
+            <p>Поможем изучать иностранные языки при помощи уникальных упражнений, сгенерированных из медиа контента.</p>
+          </div>
+
+          <Image
+            className={styles.logo}
+            src="/r_b_3.svg"
+            alt="Glossy Logo"
+            layout='fill'
+            objectFit='contain'
+            priority
+          />
+
+        </div>
       </div>
 
       <div className={styles.grid}>
@@ -36,10 +59,18 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Beta <span>-&gt;</span>
-          </h2>
-          <p>Sign-up for beta testing</p>
+          <div className={myFont.className}>
+            <h2>SIGN UP FOR BETA TESTING <span>&gt;</span><span>&gt;</span></h2>
+          </div>
+          <div className={styles.goButton}>
+          <Image
+            className={styles.arrowImage}
+            src="/arrow.svg"
+            alt="Glossy Logo"
+            width={24}
+            height={24}
+          />
+          </div>
         </a>
       </div>
     </main>
